3f28aca76a50114e1dfafbd12f9f1b2fc80015bf
mdtanvir200 <mdtanvir.ahmed@teamgrid.io>
2024-08-26 22:01:13 +0530

    shift request schema modified

--- server/models/shiftRequestSchema.js
+++ server/models/shiftRequestSchema.js
@@ -1,20 +1,28 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const shiftRequestSchema = new mongoose.schema({
-    employeeId: {
-        type: Schema.Types.ObjectId,
-        ref: 'employee',
-        required: true,
-    },
-    currentShift: String,
-    requestedShift: String,
-    requestDate: { type: Date, default: Date.now },
-    status: {
+const shiftRequestSchema = new Schema({
+    employeeId: { type: Schema.Types.ObjectId, ref: 'Employee', required: true },
+    currentRequestType: { type: String, required: true },
+    newRequestType: {
         type: String,
-        enum: ['pending', 'approved', 'rejected']
-    }
-
+        enum: ['regular', 'day', 'night'],
+        default: 'regular',
+        required: true
+    },
+    requestDate: { type: Date, default: Date.now },
+    requestTillDate: { type: Date, default: Date.now },
+    status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
+    description: { type: String, required: true },
+    managerComments: String,
+    hrComments: String
 });
+
+// Create the ShiftRequest Model
+const ShiftRequest = mongoose.model('shiftRequest', shiftRequestSchema);
+
+module.exports = ShiftRequest;
+
 
 const model = mongoose.model("shift", shiftRequestSchema);
 
