d473cc8bf1fd68d06df08a87f78f2dea44b1443a
sarukteamgrid <sksaruk.ali@teamgrid.io>
2024-12-09 12:43:21 +0530

    document route updated

--- server/routes/documentRoutes.js
+++ server/routes/documentRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const DocumentService = require("../controllers/documentController"); // Updated controller import
+const DocumentService = require("../controllers/documentController");
 const upload = require("../middlewares/fileUpload");
 const authJwt = require("../middlewares/authJwt");
 const checkRole = require("../middlewares/checkRole");
 
-// Route: Upload a document with recipients
 router.post(
   "/upload",
   authJwt("create"), // Middleware to verify JWT and authenticate the user
