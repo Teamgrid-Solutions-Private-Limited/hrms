b7b8fffb9d798a9eebd568bd0a8f06865a3f1d7b
mdtanvir200 <mdtanvir.ahmed@teamgrid.io>
2024-12-13 15:44:31 +0530

    in useer model hashed password is deleted

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -48,22 +48,8 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-// Password hashing middleware
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
 
-// Method to compare passwords
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
-};
+ 
 
 module.exports = mongoose.model("users", userSchema);
