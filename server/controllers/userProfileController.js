d41d2fa8ce4df1c03ac6358e2e7aa0f948387183
Md Tanvir Ahmed <mdtanvir.ahmed@teamgrid.io>
2025-01-07 11:43:26 +0530

    new functionality added to searc all user proofile

--- server/controllers/userProfileController.js
+++ server/controllers/userProfileController.js
@@ -175,6 +175,16 @@ class UserProfileController {
       res.status(500).json({ error: "Server error", details: error.message });
     }
   };
+
+  static findAll = async (req,res) =>
+  {
+    try {
+      profile = await UserProfile.find();
+      res.status(200).json({message: "All user profile retrieve successfully",info:profile});
+    } catch (error) {
+      res.status(500).json({message:"server error",error:error.message});
+    }
+  }
 }
 
 module.exports = UserProfileController;
